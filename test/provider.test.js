9de2ea283946a4edb48f874c9c91531f8a97e7c6
Littler <admin@sixming.com>
2022-02-18 13:39:01 +0800

    fix(test): test bug in node

--- test/provider.test.js
+++ test/provider.test.js
@@ -8,12 +8,6 @@ test(
     })
     expect(provider.constructor.name).toEqual('Provider')
 
-    expect(
-      await provider.request({
-        method: 'cfx_getBalance',
-      })
-    ).toEqual('0x3e8ef795d71dfd3ca')
-
     await expect(provider.request({})).rejects.toThrow('Method is required')
 
     await expect(provider.request(1)).rejects.toThrow(
