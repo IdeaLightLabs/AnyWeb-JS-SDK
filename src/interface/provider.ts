8d1886ce6cbdef6c557eb5963e1cf7660ab1b77d
Littleor <me@littleor.cn>
2022-05-06 15:27:01 +0800

    fix(params as any): params as any

--- src/interface/provider.ts
+++ src/interface/provider.ts
@@ -9,7 +9,7 @@ import { ConsoleLike } from '../utils/types'
  */
 export interface IRequestArguments {
   readonly method: string
-  readonly params?: IRequestParams
+  readonly params?: any
   readonly chainId?: number
 }
 
